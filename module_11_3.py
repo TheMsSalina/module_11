540c1596b6a63f35f5d5bd31d6c3ab6d9dff08d1
TheMsSalina <salinar@inbox.ru>
2024-11-30 21:58:09 +0300

    Update module_11_3.py

--- module_11_3.py
+++ module_11_3.py
@@ -2,7 +2,8 @@
 def introspection_info(obj):
     obj_type = type(obj).__name__
 
-    attributes = dir(obj)
+    if callable(obj) is False:
+        attributes = dir(obj)
 
     methods = [method for method in attributes if callable(getattr(obj, method))]
 
